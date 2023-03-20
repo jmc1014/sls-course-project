4039611fcf546944611bd647e0638f873f475404
James Carlos <jcarlos@edukasyon.ph>
2023-03-20 22:47:56 +0800

    Add Place bid service

--- sls-auction-service/src/handlers/getAuction.js
+++ sls-auction-service/src/handlers/getAuction.js
@@ -4,9 +4,8 @@ import createError from "http-errors";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-async function getAuction(event, context) {
+export async function getAuctionById(id) {
   let auction;
-  const { id } = event.pathParameters;
 
   try {
     const result = await dynamodb
@@ -21,10 +20,16 @@ async function getAuction(event, context) {
     console.log(error);
     throw new createError.InternalServerError(error);
   }
-
   if (!auction) {
     throw new createError.NotFound(`Auction with ID "${id}" not found`);
   }
+
+  return auction;
+}
+
+async function getAuction(event, context) {
+  const { id } = event.pathParameters;
+  let auction = await getAuctionById(id);
 
   return {
     statusCode: 200,
